cd2891d69222352eb920b5b123b7e108a4d19496
Rohit Pratti <49383600+marth00165@users.noreply.github.com>
2026-06-30 14:27:48 -0700

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let buckets = [];
+        let numMap = new Map();
+        let answer = [];
+
+
+
+        for (let i = 0; i <= nums.length; i++) {
+            buckets.push([])
+            let num = nums[i];
+            if (num !== undefined) {
+            numMap.set(num, (numMap.get(num) ?? 0) + 1)
+            }
+        }
+
+        for (let [num, count] of numMap) {
+            buckets[count].push(num)
+        }
+
+        for (let f = buckets.length -1; f>0; f--) {
+            let bucket = buckets[f];
+            while (bucket.length > 0) {
+                let num = bucket.pop();
+                answer.push(num);
+                k--
+                if (k=== 0) {
+                    return answer;
+                }
+            }
+        }
+
+    }
+}
